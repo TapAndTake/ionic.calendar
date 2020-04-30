98d1db1ff5c08f9e43a54b38329810df3996add5
KirillDemchenko <kirildemchenko07@gmail.com>
2020-04-30 23:12:51 +0300

    add ionic.calendar

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
+import { RouterModule } from '@angular/router';
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { CalendarModule } from 'ion2-calendar';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ]),
+    CalendarModule,
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [{provide: LOCALE_ID, useValue: 'uk' }]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CalendarComponentOptions, CalendarMonth, DayConfig } from 'ion2-calendar';
+import { CalendarService } from 'ion2-calendar/dist/services/calendar.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,59 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // dateRange: { from: string; to: string; };
+  type: 'string';
+  date = new Date;
+  month = {
+    '0':'Янв',
+    '1':'Фев',
+    '2':'Мар',
+    '3':'Апр',
+    '4':'Май',
+    '5':'Июн',
+    '6':'Июл',
+    '7':'Авг',
+    '8':'Сен',
+    '9':'Окт',
+    '10':'Ноя',
+    '11':'Дек',
+  };
+  
+  // currentMonth = this.date.getMonth();
+  currentMonth;
+  _daysConfig: DayConfig[] = [];
 
-  constructor() {}
+  constructor() {
+  
+  }
+  optionsRange: CalendarComponentOptions = {
+    monthFormat: 'DD.MM.YYYY',
+    weekdays: [
+      'Вc',
+      'Пy', 
+      'Вт',
+      'Ср',
+      'Чn',
+      'Пт',
+      'Сб',
+    ],
+    monthPickerFormat: [    
+    'Янв',
+    'Фев',
+    'Мар',
+    'Апр',
+    'Май',
+    'Июн',
+    'Июл',
+    'Авг',
+    'Сен',
+    'Окт',
+    'Ноя',
+    'Дек',
+
+    ],
+    weekStart: 1, 
+  }
+
 
 }
